59d8a8f3e3b29705f22e5acdabdca0a6806c4869
dreamdev <ujw97@naver.com>
2022-03-29 19:06:15 +0900

    프로그래머스 > level1 > 체육복

---
+++ 프로그래머스/Level1/체육복.js
@@ -0,0 +1,26 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42862
+function solution(n, lost, reserve) {
+  const students = {};
+  let answer = 0;
+  for(let i = 1; i <= n; i++){
+    students[i] = 1;
+  }
+  lost.forEach(number => students[number] -= 1);
+  reserve.forEach(number => students[number] += 1);
+
+  for(let i = 1; i <= n; i++){
+    if(students[i] === 2 && students[i-1] === 0){
+      students[i-1]++;
+      students[i]--;
+    } else if(students[i] === 2 && students[i+1] === 0){
+      students[i+1]++;
+      students[i]--;
+    }
+  }
+  for(let key in students){
+    if(students[key] >= 1){
+      answer++;
+    }
+  }
+  return answer;
+}
