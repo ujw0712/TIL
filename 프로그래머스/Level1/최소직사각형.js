d3bcd3d8306183a8a3f6a3467ff2d6e49ec3919a
jw12 <ujw97@naver.com>
2022-04-06 23:58:59 +0900

    프로그래머스 > Level1 > 최소직사각형

---
+++ 프로그래머스/Level1/최소직사각형.js
@@ -0,0 +1,5 @@
+// https://programmers.co.kr/learn/courses/30/lessons/86491
+function solution(sizes) {
+    const [hor, ver] = sizes.reduce(([h, v], [a, b]) => [Math.max(h, Math.max(a, b)), Math.max(v, Math.min(a, b))], [0, 0])
+    return hor * ver;
+}
