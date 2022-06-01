ec452dd563e416ffdfee31884d95eff419f0e473
jw12 <ujw97@naver.com>
2022-06-01 12:11:06 +0900

    프로그래머스 > level2 > 오픈채팅방.

---
+++ 프로그래머스/Level2/오픈채팅방.js
@@ -0,0 +1,25 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42888?language=javascript
+function solution(record) {
+	const userInfo = {}
+	const action = []
+	const stateMapping = {
+		'Enter': '님이 들어왔습니다.',
+		'Leave': '님이 나갔습니다.'
+	}
+
+	record.forEach((v) => {
+		const [state, id, nick] = v.split(' ')
+
+		if(state !== "Change") {
+			action.push([state, id]);
+		}
+
+		if(nick) {
+			userInfo[id] = nick
+		}
+	})
+
+	return action.map(([state, uid]) => {
+		return `${userInfo[uid]}${stateMapping[state]}`
+	})
+}
