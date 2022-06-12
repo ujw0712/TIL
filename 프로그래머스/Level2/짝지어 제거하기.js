65cbae8c2f6c8430ef6c15f57608839befb1ada1
jw12 <ujw97@naver.com>
2022-06-12 23:07:50 +0900

    프로그래머스 > level2 > 짝지어 제거하기

---
+++ 프로그래머스/Level2/짝지어 제거하기.js
@@ -0,0 +1,11 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12973?language=javascript
+function solution(s) {
+	const stack = [];
+
+	for(let i = 0; i < s.length; i++){
+		if( !stack.length || stack[stack.length-1] !== s[i] ) stack.push(s[i]);
+		else stack.pop();
+	}
+
+	return stack.length ? 0 : 1;
+}
