067580ff09e35b7678a7e67734f08a64b4d89e6a
jw12 <ujw97@naver.com>
2022-06-14 23:48:49 +0900

    프로그래머스 > level2 > 괄호 변환

---
+++ 프로그래머스/Level2/괄호 변환.js
@@ -0,0 +1,18 @@
+// https://programmers.co.kr/learn/courses/30/lessons/60058?language=javascript
+function reverse(str) {
+	return str.slice(1, str.length - 1).split("").map((c) => (c === "(" ? ")" : "(")).join("");
+}
+
+function solution(p) {
+	if (p.length < 1) return "";
+
+	let balance = 0;
+	let pivot = 0;
+	do { balance += p[pivot++] === "(" ? 1 : -1 } while (balance !== 0);
+
+	const u = p.slice(0, pivot);
+	const v = solution(p.slice(pivot, p.length));
+
+	if (u[0] === "(" && u[u.length - 1] == ")") return u + v;
+	else return "(" + v + ")" + reverse(u);
+}
