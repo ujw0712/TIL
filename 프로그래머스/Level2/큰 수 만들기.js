c810ecfb85e430f89c7376cf7494f65f55af2fe9
jw12 <ujw97@naver.com>
2022-07-16 23:41:07 +0900

    프로그래머스 > level2 > 큰 수 만들기

---
+++ 프로그래머스/Level2/큰 수 만들기.js
@@ -0,0 +1,18 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/42883?language=javascript
+function solution(number, k) {
+	const answer = []
+	let head = 0
+	let del = k
+
+	answer.push(number[head++])
+	while(answer.length < number.length - k || head < number.length) {
+		if(del && answer[answer.length-1] < number[head]) {
+			answer.pop()
+			del--
+			continue
+		}
+		answer.push(number[head++])
+	}
+
+	return answer.slice(0, number.length - k).join('')
+}
